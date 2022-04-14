afa7687a8e1c7fff2197d0810974c7c8236dd432
Lachlan <s3896004@student.rmit.edu.au>
2022-04-14 15:55:52 +1000

    hashtable complete

--- dictionary/hashtable_dictionary.py
+++ dictionary/hashtable_dictionary.py
@@ -11,13 +11,19 @@ from dictionary.word_frequency import WordFrequency
 # ------------------------------------------------------------------------
 
 class HashTableDictionary(BaseDictionary):
+    
+    def __init__(self):
+        self.object_list = None
+        self.word_frequencies = dict()
 
     def build_dictionary(self, words_frequencies: List[WordFrequency]):
         """
         construct the data structure to store nodes
         @param words_frequencies: list of (word, frequency) to be stored
         """
-        # TO BE IMPLEMENTED
+        self.object_list = words_frequencies
+        for x in self.object_list:
+            self.word_frequencies[x.word] = x.frequency
 
     def search(self, word: str) -> int:
         """
@@ -25,9 +31,10 @@ class HashTableDictionary(BaseDictionary):
         @param word: the word to be searched
         @return: frequency > 0 if found and 0 if NOT found
         """
-        # TO BE IMPLEMENTED
-        # place holder for return
-        return 0
+        if word in self.word_frequencies:
+            return self.word_frequencies[word]
+        else:
+            return 0
 
     def add_word_frequency(self, word_frequency: WordFrequency) -> bool:
         """
@@ -35,9 +42,11 @@ class HashTableDictionary(BaseDictionary):
         @param word_frequency: (word, frequency) to be added
         :return: True whether succeeded, False when word is already in the dictionary
         """
-        # TO BE IMPLEMENTED
-        # place holder for return
-        return False
+        if self.search(word_frequency.word) == 0:
+            self.word_frequencies[word_frequency.word] = word_frequency.frequency
+            return True
+        else:
+            return False
 
     def delete_word(self, word: str) -> bool:
         """
@@ -45,9 +54,11 @@ class HashTableDictionary(BaseDictionary):
         @param word: word to be deleted
         @return: whether succeeded, e.g. return False when point not found
         """
-        # TO BE IMPLEMENTED
-        # place holder for return
-        return False
+        if self.search(word) == 0:
+            return False
+        else:
+            del self.word_frequencies[word]
+            return True
 
     def autocomplete(self, word: str) -> List[WordFrequency]:
         """
@@ -55,6 +66,15 @@ class HashTableDictionary(BaseDictionary):
         @param word: word to be autocompleted
         @return: a list (could be empty) of (at most) 3 most-frequent words with prefix 'word'
         """
-        # TO BE IMPLEMENTED
-        # place holder for return
-        return []
+        top_most_frequent = []
+        word_count = 0
+        sorted_by_frequency = {k: v for k, v in sorted(self.word_frequencies.items(), key = lambda v: v[1], reverse=True) if k.startswith(word)}
+        for x in sorted_by_frequency:
+            for z in self.object_list:
+                if x == z.word:
+                    word_count += 1
+                    top_most_frequent.append(z)
+                if word_count == 3:
+                    break
+            
+        return top_most_frequent
