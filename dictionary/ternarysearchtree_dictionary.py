097efc9d67a922dfd5e405a2c19f3b3c30b802dd
huntermuze <mbyamen@gmail.com>
2022-04-18 22:29:47 +1000

    implement tst autocomplete method (contains a bug)

--- dictionary/ternarysearchtree_dictionary.py
+++ dictionary/ternarysearchtree_dictionary.py
@@ -62,8 +62,11 @@ class TernarySearchTreeDictionary(BaseDictionary):
             return find_node.frequency
 
     def search_tst(self, cur_node: Node, cur_word: str, cur_index: int):
+        # Return none if such node does not exist (i.e., the word does not exist).
         if cur_node is None:
             return None
+        else:
+            print("SEARCH CURR " + cur_node.letter)
 
         cur_char = cur_word[cur_index]
 
@@ -133,26 +136,91 @@ class TernarySearchTreeDictionary(BaseDictionary):
         @param word: word to be autocompleted
         @return: a list (could be empty) of (at most) 3 most-frequent words with prefix 'word'
         """
-        return []
 
-    #  Print the all words using recursion (debugging purposes)
-    def print_words(self, cur_node: Node, output: str, cur_index: int):
+        # TODO NEED TO MAKE IT TAKE INTO ACCOUNT WORDS BEFORE IT.
+        print()
+        self.print_words(self.root_node, "")
+        print()
+        print(self.root_node.letter)
+        print(self.root_node.middle.letter)
+        most_frequent = []
+        words_to_ignore = []
+        children_suffixes = []
+
+        # CASE 1: If the prefix is the root node, just return the entire middle subtree.
+        if word == self.root_node.letter:
+            root_of_suffixes = self.root_node.middle
+        else:
+            root_of_suffixes = self.search_tst(self.root_node, word, 0)
+            # print(root_of_suffixes.letter)
+
+        self.get_all_children_words(root_of_suffixes, "", children_suffixes)
+        print(word)
+        print(children_suffixes)
+
+        # CASE 2: If no words start with the prefix, return an empty list.
+        if len(children_suffixes) <= 0:
+            return most_frequent
+        # CASE 3: If the prefix is itself the entire word (aka. the last letter of the prefix is an end_word).
+        elif root_of_suffixes.end_word:
+            return [WordFrequency(word, root_of_suffixes.frequency)]
+        # CASE 4: If the prefix has n usages, then find all the usages and add them to a list, selecting the three
+        # with the highest frequency.
+        else:
+            for i in range(0, 3):
+                highest_frequency = 0
+                word_to_add = None
+
+                for suffix_node in children_suffixes:
+                    if suffix_node[1] > highest_frequency and suffix_node[0] not in words_to_ignore:
+                        highest_frequency = suffix_node[1]
+                        word_to_add = suffix_node[0]
+
+                if highest_frequency != 0:
+
+                    words_to_ignore.append(word_to_add)
+                    # Check if the prefix is greater than 1 or if the prefix is the root node (in both cases,
+                    # we want to include the first letter of the prefix, as it will get chopped off in the
+                    # get_all_children_words, as the base_node [i.e., the one we start with] is not included).
+                    if len(word) > 1 or word == self.root_node.letter:
+                        # Prefix + suffix = full word.
+                        word_to_add = word[0] + word_to_add
+
+                    most_frequent.append(WordFrequency(word_to_add, highest_frequency))
+
+        return most_frequent
+
+    def get_all_children_words(self, cur_node: Node, output: str, children_suffixes: list):
         if cur_node is not None:
-            self.print_words(cur_node.middle, output + str(cur_node.letter), cur_index + 1)
-            self.print_words(cur_node.right, output, cur_index)
-            self.print_words(cur_node.left, output, cur_index)
+            print("CUR LETTER: " + cur_node.letter)
+            self.get_all_children_words(cur_node.left, output, children_suffixes)
+            self.get_all_children_words(cur_node.middle, output + str(cur_node.letter), children_suffixes)
+            self.get_all_children_words(cur_node.right, output, children_suffixes)
 
             if cur_node.end_word:
-                print(" ", (output + cur_node.letter))
+                children_suffixes.append([output + cur_node.letter, cur_node.frequency])
+
+    # Print the all words using recursion (debugging purposes)
+    def print_words(self, cur_node: Node, output: str):
+        if cur_node is not None:
+            self.print_words(cur_node.left, output)
+            self.print_words(cur_node.middle, output + str(cur_node.letter))
+            self.print_words(cur_node.right, output)
+
+            if cur_node.end_word:
+                print(" ", (output + cur_node.letter), end=' ')
 
 
 if __name__ == '__main__':
     tst = TernarySearchTreeDictionary()
     tst.build_dictionary([WordFrequency("one", 10), WordFrequency("abc", 2),
                           WordFrequency("pop", 4), WordFrequency("ono", 53)])
-    tst.print_words(tst.root_node, "", 0)
-    tst.delete_word("abc")
-    tst.delete_word("ono")
+    tst.print_words(tst.root_node, "")
+    # tst.delete_word("abc")
+    # tst.delete_word("ono")
+    # print()
+    # tst.print_words(tst.root_node, "")
     print()
-    tst.print_words(tst.root_node, "", 0)
-
+    words = tst.autocomplete("on")
+    for x in words:
+        print(x.word, x.frequency)
