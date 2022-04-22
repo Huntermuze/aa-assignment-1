4410a29500fa8915224a04fc31a8553f48fea28f
huntermuze <mbyamen@gmail.com>
2022-04-22 12:21:35 +1000

    fix bugs, clean up code and restructure

--- benchmark.py
+++ benchmark.py
@@ -1,15 +1,38 @@
 import math
 import sys
 import timeit
+from typing import List
+
 import matplotlib.pyplot as plt
-from scipy.interpolate import make_interp_spline
 import numpy as np
 import random
 from axis_pair import AxisPair
+from dictionary.base_dictionary import BaseDictionary
 from dictionary.word_frequency import WordFrequency
 from dictionary.list_dictionary import ListDictionary
 from dictionary.hashtable_dictionary import HashTableDictionary
 from dictionary.ternarysearchtree_dictionary import TernarySearchTreeDictionary
+
+n_mapped_to_int = {'50': 50, '500': 500, '1k': 1000, '2k': 2000, '5k': 5000, '10k': 10000, '50k': 50000,
+                   '100k': 100000}
+
+input_sizes = ['50', '500', '1k', '2k', '5k', '10k', '50k', '100k']
+
+
+def main():
+    # Fetch the command line arguments
+    args = sys.argv
+
+    if len(args) != 2:
+        print('Incorrect number of arguments.')
+        usage()
+
+    # search
+    command = input("Please enter a command (S, A, D, AC): ")
+    if command == 'S' or command == 'A' or command == 'D' or command == 'AC':
+        final_analysis(args[1], command)
+    else:
+        print('Unknown command.')
 
 
 def usage():
@@ -21,31 +44,63 @@ def usage():
     sys.exit(1)
 
 
-def execute_commands(argument, input_sizes, command, num_commands):
-    #word_freq_to_add = get_command_arguments(command)
+def final_analysis(agent_type: str, command: str):
+    num_commands = 1000
+    upper_bound = 10
+    total_times = []
+    axes = []
+
+    # Reverse the input_size if we are dealing with a delete command.
+    if command == 'D':
+        input_sizes.reverse()
+
+    for iteration in range(0, upper_bound):
+        total_times.append(execute_commands(agent_type, command, num_commands))
+
+    # Contains the times for the list dictionary, hashtable dictionary and the tst dictionary, respectively.
+    all_dictionaries = [[], [], []]
+    for run in total_times:
+        if agent_type == "all":
+            all_dictionaries[0].append(run[0::3])
+            all_dictionaries[1].append(run[1::3])
+            all_dictionaries[2].append(run[2::3])
+        elif agent_type == "list":
+            all_dictionaries[0].append(run)
+        elif agent_type == "hashtable":
+            all_dictionaries[1].append(run)
+        else:
+            all_dictionaries[2].append(run)
+
+    for dictionary_time in all_dictionaries:
+        if len(dictionary_time) > 0:
+            axes.append(AxisPair(input_sizes, np.average(np.array(dictionary_time), axis=0)))
+
+    plot_graph(axes, input_sizes[0], input_sizes[-1], command, num_commands)
+
+
+def execute_commands(agent_type: str, command: str, num_commands: int) -> list:
+    # word_freq_to_add = get_command_arguments(command)
     times = []
 
     for (i, n) in enumerate(input_sizes):
-        agents = get_agents(argument)
-        word_freq_to_add = get_command_random(command, get_word_freq_list(n), n, num_commands)
+        agents = get_agents(agent_type)
+        word_freq_to_add = get_command_random(command, get_input_from_file("input/input_" + n, True), n, num_commands)
 
         for (j, agent) in enumerate(agents):
             avg = 0
-
-            agent.build_dictionary(get_word_freq_list(n))
+            agent.build_dictionary(get_input_from_file("input/input_" + n, True))
 
             if command == 'S':
-                for x in word_freq_to_add:
-                    avg += timeit.timeit(lambda: agent.search(x), number=1) * 1000 * 1000 * 1000
+                method_to_time = agent.search
             elif command == 'A':
-                for x in word_freq_to_add:
-                    avg += timeit.timeit(lambda: agent.add_word_frequency(x), number=1) * 1000 * 1000 * 1000
+                method_to_time = agent.add_word_frequency
             elif command == 'D':
-                for x in word_freq_to_add:
-                    avg += timeit.timeit(lambda: agent.delete_word(x), number=1) * 1000 * 1000 * 1000
-            elif command == 'AC':
-                for x in word_freq_to_add:
-                    avg += timeit.timeit(lambda: agent.autocomplete(x), number=1) * 1000 * 1000 * 1000
+                method_to_time = agent.delete_word
+            else:
+                method_to_time = agent.autocomplete
+
+            for x in word_freq_to_add:
+                avg += timeit.timeit(lambda: method_to_time(x), number=1) * 1000 * 1000 * 1000
 
             num_commands = len(word_freq_to_add)
             times.append(math.log(avg / num_commands, 10))
@@ -53,173 +108,80 @@ def execute_commands(argument, input_sizes, command, num_commands):
 
     return times
 
-def get_command_random(command, input_file, n, num_commands):
-    words_frequencies_from_file = []
-    # scenario 1 grow
+
+def get_agents(agent_type: str) -> List[BaseDictionary]:
+    if agent_type == 'list':
+        return [ListDictionary()]
+    elif agent_type == 'hashtable':
+        return [HashTableDictionary()]
+    elif agent_type == 'tst':
+        return [TernarySearchTreeDictionary()]
+    elif agent_type == 'all':
+        return [ListDictionary(), HashTableDictionary(), TernarySearchTreeDictionary()]
+    else:
+        print('Incorrect argument value.')
+        usage()
+
+
+def get_input_from_file(file_path: str, create_word_frequency: bool) -> list:
+    input_from_file = []
+
+    with open(file_path, 'r') as data_file:
+        for line in data_file:
+            values = line.split()
+            word = values[0]
+
+            # If each line contains a word and its frequency
+            if create_word_frequency:
+                frequency = int(values[1])
+                input_from_file.append(WordFrequency(word, frequency))
+            # If there is only a word and no frequency (i.e., delete, search or autocomplete commands).
+            else:
+                input_from_file.append(word)
+
+    return input_from_file
+
+
+def get_command_random(command: str, word_frequencies_from_file: List[WordFrequency], n: str,
+                       num_commands: int) -> list:
+    command_input = []
+    max_num = n_mapped_to_int[n]
+    store_max = 0
+
+    # TODO randomise autocomplete too, then tidy this up.
+
+    # Scenario 1 grow
     if command == 'A':
-        if n == '50':
-            max_num = 50
-        elif n == '500':
-            max_num = 500
-        elif n == '1k':
-            max_num = 1000
-        elif n == '2k':
-            max_num = 2000
-        elif n == '5k':
-            max_num = 5000
-        elif n == '10k':
-            max_num = 10000
-        elif n == '50k':
-            max_num = 50000
-        elif n == '100k':
-            max_num = 100000
         for i in range(0, num_commands):
             store_max = i
-            words_frequencies_from_file.append(input_file[random.randint(0, max_num-1)])
+            command_input.append(word_frequencies_from_file[random.randint(0, max_num - 1)])
         print("The max num is: " + str(store_max + 1))
-        return words_frequencies_from_file
-    # scenario 2 shrink + scenario 3 search
+    # Scenario 2 shrink + Scenario 3 search
     elif command == 'D' or command == 'S':
-        if n == '50':
-            max_num = 50
-        elif n == '500':
-            max_num = 500
-        elif n == '1k':
-            max_num = 1000
-        elif n == '2k':
-            max_num = 2000
-        elif n == '5k':
-            max_num = 5000
-        elif n == '10k':
-            max_num = 10000
-        elif n == '50k':
-            max_num = 50000
-        elif n == '100k':
-            max_num = 100000
         for i in range(0, 1000):
             store_max = i
-            words_frequencies_from_file.append(input_file[random.randint(0, max_num-1)].word)
+            command_input.append(word_frequencies_from_file[random.randint(0, max_num - 1)].word)
         print("The max num is: " + str(store_max + 1))
-        return words_frequencies_from_file
-    #scenario 3 autocomplete
-    elif command == 'AC':
-        data_file = open("commands/20_commands_autocomplete", 'r')
-        for line in data_file:
-            values = line.split()
-            word = values[0]
-            words_frequencies_from_file.append(word)
-        data_file.close()
-        return words_frequencies_from_file
-
-
-def get_command_arguments(command):
-    words_frequencies_from_file = []
-    # scenario 1
-    if command == 'A':
-        data_file = open("commands/20_commands_add", 'r')
-        for line in data_file:
-            values = line.split()
-            word = values[0]
-            frequency = int(values[1])
-            word_frequency = WordFrequency(word, frequency)  # each line contains a word and its frequency
-            words_frequencies_from_file.append(word_frequency)
-        data_file.close()
-        return words_frequencies_from_file
-    # scenario 2
-    elif command == 'D':
-        data_file = open("commands/20_commands_delete", 'r')
-        for line in data_file:
-            values = line.split()
-            word = values[0]
-            words_frequencies_from_file.append(word)
-        data_file.close()
-        return words_frequencies_from_file
-    # scenario 3
-    elif command == 'S':
-        data_file = open("commands/20_commands_search", 'r')
-        for line in data_file:
-            values = line.split()
-            word = values[0]
-            words_frequencies_from_file.append(word)
-        data_file.close()
-        return words_frequencies_from_file
-    elif command == 'AC':
-        data_file = open("commands/20_commands_autocomplete", 'r')
-        for line in data_file:
-            values = line.split()
-            word = values[0]
-            words_frequencies_from_file.append(word)
-        data_file.close()
-        return words_frequencies_from_file
-
-
-def get_word_freq_list(n):
-    words_frequencies_from_file = []
-    data_file = open("input/input_" + n, 'r')
-    for line in data_file:
-        values = line.split()
-        word = values[0]
-        frequency = int(values[1])
-        word_frequency = WordFrequency(word, frequency)  # each line contains a word and its frequency
-        words_frequencies_from_file.append(word_frequency)
-    data_file.close()
-    return words_frequencies_from_file
-
-
-def final_analysis(argument, input_sizes, command):
-    num_commands = 1000
-    total_times = []
-    axes = []
-    upper_bound = 10
-
-    for iteration in range(0, upper_bound):
-        total_times.append(execute_commands(argument, input_sizes, command, num_commands))
-
-    # Contains the times for the list dictionary, hashtable dictionary and the tst dictionary, respectively.
-    all_dictionaries = [[], [], []]
-    for run in total_times:
-        if argument == "all":
-            all_dictionaries[0].append(run[0::3])
-            all_dictionaries[1].append(run[1::3])
-            all_dictionaries[2].append(run[2::3])
-        elif argument == "list":
-            all_dictionaries[0].append(run)
-        elif argument == "hashtable":
-            all_dictionaries[1].append(run)
-        else:
-            all_dictionaries[2].append(run)
-
-    if len(all_dictionaries[0]) > 0:
-        axes.append(AxisPair(input_sizes, np.average(np.array(all_dictionaries[0]), axis=0)))
-    if len(all_dictionaries[1]) > 0:
-        axes.append(AxisPair(input_sizes, np.average(np.array(all_dictionaries[1]), axis=0)))
-    if len(all_dictionaries[2]) > 0:
-        axes.append(AxisPair(input_sizes, np.average(np.array(all_dictionaries[2]), axis=0)))
-
-    # numeric_input_sizes = np.array([50, 500, 1000, 2000, 5000, 10000, 50000, 100000])
-    # x = np.linspace(numeric_input_sizes.min(), numeric_input_sizes.max(), 300)
-    #
-    # spl = make_interp_spline(numeric_input_sizes, times, k=3)
-    # graph_smooth = spl(x)
-    # plot_graph([AxisPair(x, graph_smooth)])
-
-    plot_graph(axes, input_sizes[0], input_sizes[-1], command, num_commands)
-
-
-def plot_graph(axes, x_axis_min, x_axis_max, command, num_commands):
-    graph_title = ''
-    if len(axes) > 0:
-        for idx, axes_pair in enumerate(axes):
-            title = "List"
-
-            if idx == 1:
-                title = "Hashtable"
-            elif idx == 2:
-                title = "TST"
-
-            plt.plot(axes_pair.x_axis, axes_pair.y_axis, label=title)
+    # Scenario 3 autocomplete
     else:
+        command_input = get_input_from_file("commands/20_commands_autocomplete", False)
+
+    return command_input
+
+
+def plot_graph(axes: List[AxisPair], x_axis_min: str, x_axis_max: str, command: str, num_commands: int):
+    if len(axes) <= 0:
         return
+
+    for idx, axes_pair in enumerate(axes):
+        title = "List"
+
+        if idx == 1:
+            title = "Hashtable"
+        elif idx == 2:
+            title = "TST"
+
+        plt.plot(axes_pair.x_axis, axes_pair.y_axis, label=title)
 
     if command == 'S':
         graph_title = 'Search Benchmark'
@@ -227,7 +189,7 @@ def plot_graph(axes, x_axis_min, x_axis_max, command, num_commands):
         graph_title = 'Add Benchmark'
     elif command == 'D':
         graph_title = 'Delete Benchmark'
-    elif command == 'AC':
+    else:
         graph_title = 'Autocomplete Benchmark'
 
     plt.xlabel('Number of Elements before Operation')
@@ -238,34 +200,26 @@ def plot_graph(axes, x_axis_min, x_axis_max, command, num_commands):
     plt.show()
 
 
-def get_agents(argument):
-    if argument == 'list':
-        return [ListDictionary()]
-    elif argument == 'hashtable':
-        return [HashTableDictionary()]
-    elif argument == 'tst':
-        return [TernarySearchTreeDictionary()]
-    elif argument == 'all':
-        return [ListDictionary(), HashTableDictionary(), TernarySearchTreeDictionary()]
-    else:
-        print('Incorrect argument value.')
-        usage()
+# TODO Scheduled for deletion.
+# def get_command_arguments(command: str) -> list:
+#     create_word_frequency = False
+#     file_name = "commands/"
+#
+#     # Scenario 1
+#     if command == 'A':
+#         file_name += "20_commands_add"
+#         create_word_frequency = True
+#     # Scenario 2
+#     elif command == 'D':
+#         file_name += "20_commands_delete"
+#     # Scenario 3
+#     elif command == 'S':
+#         file_name += "20_commands_search"
+#     else:
+#         file_name += "20_commands_autocomplete"
+#
+#     return get_input_from_file(file_name, create_word_frequency)
 
 
 if __name__ == '__main__':
-    input_sizes_grow = ['50', '500', '1k', '2k', '5k', '10k', '50k', '100k']
-    input_sizes_shrink = ['100k', '50k', '10k', '5k', '2k', '1k', '500', '50']
-
-    # Fetch the command line arguments
-    args = sys.argv
-
-    if len(args) != 2:
-        print('Incorrect number of arguments.')
-        usage()
-
-    # search
-    command = input("Please enter a command (S, A, D, AC): ")
-    if command == 'S' or command == 'A' or command == 'D' or command == 'AC':
-        final_analysis(args[1], input_sizes_grow, command)
-    else:
-        print('Unknown command.')
+    main()
