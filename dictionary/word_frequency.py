cca685c3c4eaffdfba472d8299c40687419255db
huntermuze <mbyamen@gmail.com>
2022-04-22 18:36:32 +1000

    overload operators to support new optimisations

--- dictionary/word_frequency.py
+++ dictionary/word_frequency.py
@@ -9,3 +9,21 @@ class WordFrequency:
     def __init__(self, word: str, frequency: int):
         self.word = word
         self.frequency = frequency
+
+    def __gt__(self, other):
+        if isinstance(other, WordFrequency):
+            return self.word > other.word
+        elif isinstance(other, str):
+            return self.word > other
+        else:
+            raise TypeError("You cannot compare a WordFrequency with type" + str(type(other)))
+
+    def __lt__(self, other):
+        if isinstance(other, WordFrequency):
+            return self.word < other.word
+        elif isinstance(other, str):
+            return self.word < other
+        else:
+            raise TypeError("You cannot compare a WordFrequency with type" + str(type(other)))
+
+
