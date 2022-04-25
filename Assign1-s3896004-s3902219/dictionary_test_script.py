30159a33a459784751aaa83e836adc88a60cb664
huntermuze <mbyamen@gmail.com>
2022-04-25 11:32:08 +1000

    fix bug

--- Assign1-s3896004-s3902219/dictionary_test_script.py
+++ Assign1-s3896004-s3902219/dictionary_test_script.py
@@ -141,7 +141,7 @@ def main():
             (sStdout, sStderr) = proc.communicate()
 
             if bVerbose and len(sStderr) > 0:
-                print("\nWarnings and error messages from running python program:\n" + sStderr)
+                print("\nWarnings and error messages from running python program:\n" + str(sStderr))
 
             # compare expected with output
             bPassed, bFailedOutput = evaluate(sExpectedFile, sOutputFile)
